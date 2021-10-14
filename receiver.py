8d79b37238c5040ade26983393da72e5a36e8e56
Quizlock <seritechsoftware@gmail.com>
2021-10-14 12:26:39 -0500

    Got one side talking, other on a 5-12 second delay.

--- receiver.py
+++ receiver.py
@@ -11,19 +11,22 @@ class Receiver:
         try:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.sock.bind((self.ip, self.port))
+            open_socket = True
         except:
             print('Could not bind to port ' + str(self.port))
+            open_socket = False
 
-        chunk_size = 1024
-        audio_format = pyaudio.paInt16
-        channels = 1
-        rate = 20000
+        if (open_socket):
+            chunk_size = 1024
+            audio_format = pyaudio.paInt16
+            channels = 1
+            rate = 20000
 
-        self.p_aud = pyaudio.PyAudio()
-        self.playing_stream = self.p_aud.open(format=audio_format, channels=channels, rate=rate, output=True, frames_per_buffer=chunksize)
-        self.recording_stream = self.p_aud.open(format=audio_format, channels=channels, rate=rate, input=True, frames_per_buffer=chunksize)
+            self.p_aud = pyaudio.PyAudio()
+            self.playing_stream = self.p_aud.open(format=audio_format, channels=channels, rate=rate, output=True, frames_per_buffer=chunk_size)
+            self.recording_stream = self.p_aud.open(format=audio_format, channels=channels, rate=rate, input=True, frames_per_buffer=chunk_size)
 
-        self.accept_connections()
+            self.accept_connections()
 
     def accept_connections(self):
         self.sock.listen(100)
@@ -32,7 +35,7 @@ class Receiver:
 
         c, addr = self.sock.accept()
         threading.Thread(target=self.receive_data,args=(c,)).start()
-        self.send_data(c)
+        threading.Thread(target=self.send_data,args=(c,)).start()
 
 
     def receive_data(self, sock):
