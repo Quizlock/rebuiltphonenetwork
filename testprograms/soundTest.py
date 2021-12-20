be1b2485cc812fa652f51d55b99439508bfcf5a1
Quizlock <seritechsoftware@gmail.com>
2021-12-19 22:04:23 -0600

    Updated test files

--- testprograms/soundTest.py
+++ testprograms/soundTest.py
@@ -3,20 +3,49 @@ import wave
 import sys
 import time
 from gpiozero import Button
+from pad4pi import rpi_gpio
 
 CHUNK = 1024
 
-dialtone = wave.open("dialtone.wav", 'rb')
-tone0 = wave.open("busytone.wav", 'rb')
+print("Loading WAVE FILES")
+dialtone = wave.open("0.wav", 'rb')
+print("Dial Tone: ", dialtone.getnframes())
+busytone = wave.open("busytone.wav", 'rb')
+print("Busy Tone: ", busytone.getnframes())
+tone0 = wave.open("dialtone.wav", 'rb')
+print("0 Tone: ", tone0.getnframes())
 
 switch = Button(24)
+##########################
+#INITIALIZE KEYPAD
+##########################
+print("Initializing Keypad...", end="")
+KEYPAD = [ [1,2,3], [4,5,6], [7,8,9], ["*",0,"#"]]
+ROW_PINS = [9, 11, 20, 21]
+COL_PINS = [26, 19, 13]
+
+factory = rpi_gpio.KeypadFactory()
+keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PINS)
+
+def key_pressed(key):
+    print("Pressed ", key)
+    if play_tone.pressed == False:
+        play_tone.pressed = True
+        play_tone.tone = key
+
+keypad.registerKeyPressHandler(key_pressed)
+print("Done.")
+
 class OnOff:
     def __init__(self):
         self.pressed = False
+        self.tone = -1
 
 play_tone = OnOff()
 def pressed_trigger():
-    play_tone.pressed = True
+    if play_tone.pressed == False:
+        play_tone.pressed = True
+        play_tone.tone = "B"
 
 switch.when_pressed = pressed_trigger
 
@@ -25,7 +54,10 @@ pa = pyaudio.PyAudio()
 
 def callback(in_data, frame_count, time_info, status):
     if play_tone.pressed:
-        data = tone0.readframes(frame_count)
+        if play_tone.tone == "B":
+            data = busytone.readframes(frame_count)
+        else:
+            data = tone0.readframes(frame_count)
     else:
         data = dialtone.readframes(frame_count)
     return (data, pyaudio.paContinue)
@@ -37,7 +69,7 @@ stream.start_stream()
 
 while True:
     if stream.is_active():
-        time.sleep(0.01) 
+        time.sleep(0.1) 
     else:
         print("Stream ended")
         play_tone.pressed = False
